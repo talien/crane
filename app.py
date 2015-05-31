6a80e886898dd6372bcc5f20d4c6e503741b4e20
Sándor Irházi <wuxort@gmail.com>
2015-05-31 18:55:12 +0000

    app.py: PEP-8.

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from crane.webserver import app, db
 import argparse
 
+
 def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--listen-ip", dest="ip", default="127.0.0.1")
-  args = parser.parse_args()
-  db.create_all()
-  app.run(debug=True, host=args.ip)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--listen-ip", dest="ip", default="127.0.0.1")
+    args = parser.parse_args()
+    db.create_all()
+    app.run(debug=True, host=args.ip)
 
 main() 
