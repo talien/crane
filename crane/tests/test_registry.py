765b7adfe31af41777a4df58b28d81b32f7d5439
Sándor Irházi <wuxort@gmail.com>
2015-07-13 20:47:14 +0000

    test_registry.py: Added the missing tests for CommonRegistry.

--- crane/tests/test_registry.py
+++ crane/tests/test_registry.py
@@ -47,6 +47,17 @@ class TestCommonRegistry:
         assert(common_registry().username == username)
         assert(common_registry().password == password)
 
+    def test_search(self):
+        with pytest.raises(NotImplementedError):
+            common_registry().search("a")
+
+    def test_image(self):
+        with pytest.raises(NotImplementedError):
+            common_registry().image("a", "b")
+
+    def test_tags(self):
+        with pytest.raises(NotImplementedError):
+            common_registry().tags("a")
 
 class TestDockerHub:
     def test_query_tags(self):
